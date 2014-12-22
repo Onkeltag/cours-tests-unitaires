107eb06fa7daeebf44f7aaf5987d77ca0dfa06cb
Tag <jrmcheron@gmail.com>
2014-12-22 17:00:36 +0100

    prmeier test corrigé assert

--- tests.js
+++ tests.js
@@ -2,6 +2,7 @@ QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
 
-test('InitialisationApp', function() {
-var jeu = new Jeu() ;
+test('InitialisationApp', function( assert ) {
+	assert.expect(0);
+	var jeu = new Jeu() ;
 });
