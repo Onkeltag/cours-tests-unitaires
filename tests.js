b927ff3c44b6186bf2a967d61c72190ba3a674ea
Tag <jrmcheron@gmail.com>
2014-12-22 16:55:36 +0100

    1er test

--- tests.js
+++ tests.js
@@ -2,6 +2,6 @@ QUnit.test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
 
-QUnit.test( "Game Test", function( assert ) {
-  assert.ok( game, "Passed!" );
+test('InitialisationApp', function() {
+var jeu = new Jeu() ;
 });
