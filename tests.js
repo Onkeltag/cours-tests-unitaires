7ffa500f12128e252d7e7830b35fd5a46b4b98be
Tag <jrmcheron@gmail.com>
2014-12-23 09:44:43 +0100

    test score debut de partie

--- tests.js
+++ tests.js
@@ -6,3 +6,8 @@ test('InitialisationApp', function( assert ) {
 	assert.expect(0);
 	var jeu = new Jeu() ;
 });
+
+test('AfficherScoreDebutDePartie', function() {
+var jeu = new Jeu() ;
+equal("0-0",jeu.score()) ;
+});
