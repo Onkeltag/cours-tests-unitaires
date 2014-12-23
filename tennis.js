8c15eb0071a974e362171b34c27d1f7a5aa07137
WoufWouf <ludovic.garraud@gmail.com>
2014-12-23 14:09:17 +0100

    Programme à la pause

--- tennis.js
+++ tennis.js
@@ -1,1 +1,67 @@
-function Jeu(){ };
+function Jeu(){
+	this.joueurs = ['Le joueur A','Le joueur B'];
+
+	this.marque = [0,0];	
+}
+
+Jeu.prototype.score = function () {
+		try {
+			if ((this.marque[0] === 0 || this.marque[0] === 15 || this.marque[0] === 30 || this.marque[0] === 40 || this.marque[0] === 50) &&
+				(this.marque[1] === 0 || this.marque[1] === 15 || this.marque[1] === 30 || this.marque[1] === 40 || this.marque[1] === 50)) {
+				if (this.marque[0] === this.marque[1] && this.marque[0] !== 40 && this.marque[0] !== 0) {
+					return this.marque[0] + 'A';
+				} else if (this.marque[0] === this.marque[1] && this.marque[0] === 40) {
+					return 'Egalité';
+				} else if (this.marque[0] === 50) {
+					return 'Avantage ' + this.joueurs[0];
+				} else if (this.marque[1] === 50) {
+					return 'Avantage ' + this.joueurs[1];
+				} else {
+				return this.marque[0] + '-' + this.marque[1];
+				}
+			}
+			else {
+				throw 'score non conforme';
+			}
+		}
+		catch (err) {
+			return 'Erreur : ' + err;
+		}
+	}
+
+Jeu.prototype.addPoint = function(id) {
+		//Le joueur joueurs[id] marque un point :
+		if (this.marque[id] === 0 || this.marque[id] === 15) {
+			//Situtation classique
+			this.marque[id] += 15;
+		}
+		else if(this.marque[id] === 30) {
+			//Situation à 30 - xx
+			this.marque[id] += 10;
+		}
+		else if(this.marque[id] === 40) {
+			//Situation à 40 - xx
+			if (this.marque[id] > this.marque[1-id]) {
+				//Situation de victoire
+				console.log('jeu ' + this.joueurs[id]);
+			}
+			else if (this.marque[0] === this.marque[1]) {
+				//Situation d'égalité
+				this.marque[id] = 50;
+			}
+			else if (this.marque[1-id] === 50) {
+				//Situation d'avantage pour l'adversaire
+				this.marque[1-id] = 40;
+			}
+			else if(this.marque[id] === 50) {
+				//Situation d'avantage
+				return ('jeu ' + this.joueurs[id]);
+			}
+		}
+	}	
+
+var jeu = new Jeu();
+
+$('#itsInA').click(jeu.addPoint(0));
+$('#itsInB').click(jeu.addPoint(1));
+$('#score').html(jeu.score());
--- tests.js
+++ tests.js
@@ -6,8 +6,58 @@ test('InitialisationApp', function( assert ) {
 	assert.expect(0);
 	var jeu = new Jeu() ;
 });
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+test('AfficherScoreDebutDePartie', function() { var jeu = new Jeu() ;
+equal("0-0",jeu.score()) ; });
+
+test('AfficherScore', function() { var jeu = new Jeu() ;
+	jeu.marque=[0,15]; equal("0-15",jeu.score()) ; 
+	jeu.marque=[0,30]; equal("0-30",jeu.score()) ; 
+	jeu.marque=[0,40]; equal("0-40",jeu.score()) ; 
+	jeu.marque=[15,0]; equal("15-0",jeu.score()) ; 
+	jeu.marque=[30,0]; equal("30-0",jeu.score()) ; 
+	jeu.marque=[40,0]; equal("40-0",jeu.score()) ; 
+	jeu.marque=[15,15]; equal("15A",jeu.score()) ; 
+	jeu.marque=[15,30]; equal("15-30",jeu.score()) ; 
+	jeu.marque=[15,40]; equal("15-40",jeu.score()) ; 
+	jeu.marque=[30,15]; equal("30-15",jeu.score()) ; 
+	jeu.marque=[40,15]; equal("40-15",jeu.score()) ;
+	jeu.marque=[30,30]; equal("30A",jeu.score()) ;
+	jeu.marque=[40,30]; equal("40-30",jeu.score()) ; 
+	jeu.marque=[30,40]; equal("30-40",jeu.score()) ; 
+	jeu.marque=[40,40]; equal("Egalité",jeu.score()) ; 
+	jeu.marque=[40,50]; equal("Avantage Le joueur B",jeu.score()) ;   
+	jeu.marque=[50,40]; equal("Avantage Le joueur A",jeu.score()) ;  
+	jeu.marque=[12,15]; equal("Erreur : score non conforme",jeu.score()) ;
+	jeu.marque=[0,15.5]; equal("Erreur : score non conforme",jeu.score()) ; 
+	jeu.marque=['quinze','quinze']; equal("Erreur : score non conforme",jeu.score()) ; 
+});
+test('AjouterPoint', function() {
+	var jeu = new Jeu();
+	jeu.addPoint(0) ; equal(15,jeu.marque[0]);
+	jeu.addPoint(1) ; equal(15,jeu.marque[1]);
+	jeu.addPoint(0) ; equal(30,jeu.marque[0]);
+	jeu.addPoint(1) ; equal(30,jeu.marque[1]);	
+	jeu.addPoint(1) ; equal(40,jeu.marque[1]);
+	jeu.addPoint(0) ; equal(40,jeu.marque[0]);
+	jeu.addPoint(1) ; equal(50,jeu.marque[1]);
+	jeu.addPoint(1) ; equal('jeu Le joueur B');
+
+})
+
+
+//test('TestFonctionSave', function() { } )
+=======
+>>>>>>> Stashed changes
 
 test('AfficherScoreDebutDePartie', function() {
 var jeu = new Jeu() ;
 equal("0-0",jeu.score()) ;
+<<<<<<< Updated upstream
 });
+=======
+});
+>>>>>>> FETCH_HEAD
+>>>>>>> Stashed changes
